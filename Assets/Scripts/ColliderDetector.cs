b2bbe1b149039189c98965bf4dbb28def3b0b188
masterfermin02 <fperdomo@intellisys.com.do>
2020-06-23 11:05:22 -0400

    Added Collisions project

---
+++ Assets/Scripts/ColliderDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDetector : MonoBehaviour
+{
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("An object with name " + collision.gameObject.name + " HAS COLLISIONED with me");
+	}
+
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		Debug.Log("An object with name " + collision.gameObject.name + " IS COLLISIONING with me");
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		Debug.Log("An object with name " + collision.gameObject.name + " ISN'T COLLISIONING with me  anymore");
+	}
+}
---
+++ Assets/Scripts/TriggerDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetector : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Another collider with name " + other.gameObject.name + " HAS ENTERED in my area");
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		Debug.Log("Another collider with name " + other.gameObject.name + " IS in my area");
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		Debug.Log("Another collider with name " + other.gameObject.name + " HAS LEFT my area");
+	}
+}
